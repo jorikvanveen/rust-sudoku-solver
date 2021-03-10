36551e6b57432aa986b29d01ea3d202a6276110f
Jorik <vanveenjorik@gmail.com>
2021-03-10 12:54:53 +0100

    Added wasm folder to repo

---
+++ public/wasm/bettersudoku_bg.wasm.d.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function solve(a: number, b: number, c: number): void;
+export function __wbgt_construct_sudoku_0(a: number): void;
+export function __wbgt_solve_sudoku_1(a: number): void;
+export function __wbg_wasmbindgentestcontext_free(a: number): void;
+export function wasmbindgentestcontext_new(): number;
+export function wasmbindgentestcontext_args(a: number, b: number, c: number): void;
+export function wasmbindgentestcontext_run(a: number, b: number, c: number): number;
+export function __wbgtest_console_log(a: number): void;
+export function __wbgtest_console_debug(a: number): void;
+export function __wbgtest_console_info(a: number): void;
+export function __wbgtest_console_warn(a: number): void;
+export function __wbgtest_console_error(a: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h1a66a6c0e4ad3dd5(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__hfd9e5a8cb33864ee(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke3_mut__h45c3d04a217ec338(a: number, b: number, c: number, d: number, e: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h2778f1b6137224df(a: number, b: number, c: number, d: number): void;
